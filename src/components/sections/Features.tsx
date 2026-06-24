762464846aea5e626acddc52923f8b54baa22349
Tamarakro97 <tamarprebor@gmail.com>
2026-06-24 21:10:22 +0100

    made another change

--- src/components/sections/Features.tsx
+++ src/components/sections/Features.tsx
@@ -36,7 +36,7 @@ const container = {
 }
 const item = {
   hidden: { opacity: 0, y: 24 },
-  show:   { opacity: 1, y: 0, transition: { duration: 0.5, ease: [0.34, 1.56, 0.64, 1] } },
+  show:   { opacity: 1, y: 0, transition: { duration: 0.5, ease: [0.34, 1.56, 0.64, 1] as [number, number, number, number] } },
 }
 
 export function Features() {
